fc210d3e89366a3eb937c55a7b430be63605b979
Paulo Henrique <paulohenriquepaiva@ymail.com>
2020-08-14 05:52:06 -0300

    Alterações

--- ScrabbleCounter/src/pages/Home/Tab1.tsx
+++ ScrabbleCounter/src/pages/Home/Tab1.tsx
@@ -6,9 +6,9 @@ import points from '../../Points/pt-br';
 
 class Tab1 extends Component {
   state = {
-    letters: [],
     currentPlayer: "",
     currentWord: "",
+    statusGame: true,
     results: [
       { name: "Paulo", points: 0 },
       { name: 'Daniela', points: 0 }
@@ -54,8 +54,27 @@ class Tab1 extends Component {
     console.log(this.state.results)
   }
 
-  showResults() {
-    console.log(this.state.results)
+  showResults(choice: number) {
+    let results = []
+    if(choice === 0) {
+      results = this.state.results
+      results.sort((a, b) => {
+        if (a.points > b.points) {
+          return -1;
+        }
+        if (a.points < b.points) {
+          return 1;
+        }
+        return 0;
+      })
+      this.setState({results: results, statusGame: false})
+    } else {
+      results = [
+        { name: "Paulo", points: 0 },
+        { name: 'Daniela', points: 0 }
+      ]
+      this.setState({results: results, statusGame: true, currentPlayer: "", currentWord: ""})
+    }
   }
 
   setCurrentPlayer(name: string) {
@@ -67,53 +86,75 @@ class Tab1 extends Component {
       <IonPage>
       <IonContent>
         <IonGrid>
-          <IonRow className="ion-justify-content-center margin20">
-            {this.state.results.map((result, i) => {
-              return(
-                <IonButton 
-                  key={i}
-                  fill="clear"
-                  onClick={() => this.setCurrentPlayer(result.name)}>
-                  {result.name}
+          {this.state.statusGame ? 
+            <div>
+              <IonRow className="ion-justify-content-center margin20">
+                {this.state.results.map((result, i) => {
+                  return(
+                    <IonButton 
+                      key={i}
+                      fill="clear"
+                      onClick={() => this.setCurrentPlayer(result.name)}>
+                      {result.name}
+                    </IonButton>
+                  )
+                })}
+              </IonRow>
+              <IonRow className="ion-justify-content-center">
+                <IonText color="dark">
+                  <h5>
+                    Jogador atual: {this.state.currentPlayer !== '' ? this.state.currentPlayer : 'Nenhum'}<br />
+                  </h5>
+                </IonText>
+              </IonRow>
+              <IonRow className="ion-justify-content-center ion-align-items-center">
+                <IonText color="dark" style={{ width: '65%', height: 35, backgroundColor: '#EEEEEE'}}>
+                  <h4 style={{ marginTop: 5 }}>{this.state.currentWord}</h4>
+                </IonText>
+                <IonButton color="danger" onClick={() => this.removePoint()}>
+                  <IonIcon slot="icon-only" icon={backspace} />
                 </IonButton>
-              )
-            })}
-          </IonRow>
-          <IonRow className="ion-justify-content-center">
-            <IonText color="dark">
-              <h5>
-                Jogador atual: {this.state.currentPlayer !== '' ? this.state.currentPlayer : 'Nenhum'}<br />
-              </h5>
-            </IonText>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center">
-            <IonText color="dark" style={{ width: '65%', height: 35, backgroundColor: '#EEEEEE'}}>
-              <h4 style={{ marginTop: 5 }}>{this.state.currentWord}</h4>
-            </IonText>
-            <IonButton color="danger" onClick={() => this.removePoint()}>
-              <IonIcon slot="icon-only" icon={backspace} />
-            </IonButton>
-          </IonRow>
-          <IonRow className="ion-justify-content-center margin10">
-            {points.map((letter, i) => {
-              return(
+              </IonRow>
+              <IonRow className="ion-justify-content-center margin10">
+                {points.map((letter, i) => {
+                  return(
+                    <IonButton 
+                      onClick={() => this.setPoint(letter.value, letter.name)}
+                      style={{ marginTop: 10, marginRight: 10, marginLeft: 10}}
+                      color="light" 
+                      key={i}>
+                      { letter.name }
+                    </IonButton>
+                  )
+                })}
+              </IonRow>
+              <IonRow className="ion-justify-content-center margin20">
                 <IonButton 
-                  onClick={() => this.setPoint(letter.value, letter.name)}
-                  style={{ marginTop: 10, marginRight: 10, marginLeft: 10}}
-                  color="light" 
-                  key={i}>
-                  { letter.name }
+                  onClick={() => this.showResults(0)}
+                  color="success">
+                  Finalizar o Jogo
                 </IonButton>
-              )
-            })}
-          </IonRow>
-          <IonRow className="ion-justify-content-center margin20">
-            <IonButton 
-              onClick={() => this.showResults()}
-              color="success">
-              Finalizar Jogo
-            </IonButton>
-          </IonRow>
+              </IonRow>
+            </div> :
+            <div>
+              <IonRow className="ion-justify-content-center margin5">
+                <IonButton 
+                  onClick={() => this.showResults(1)}
+                  color="success">
+                  Recomeçar o Jogo
+                </IonButton>
+              </IonRow>
+              {this.state.results.map((result, i) => {
+                return(
+                  <IonRow key={i} className="ion-justify-content-center margin5">
+                    <IonText key={i} color="dark">
+                      <h4 key={i}>{result.name} com {result.points}</h4>
+                    </IonText>
+                  </IonRow>
+                )
+              })}
+            </div>
+          }
         </IonGrid>
       </IonContent>
     </IonPage>
