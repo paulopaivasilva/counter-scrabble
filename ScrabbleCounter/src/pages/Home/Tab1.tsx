b404509a00440310929cc103201ed627c3984177
Paulo Henrique <paulohenriquepaiva@ymail.com>
2020-08-17 03:46:08 -0300

    Alterações

--- ScrabbleCounter/src/pages/Home/Tab1.tsx
+++ ScrabbleCounter/src/pages/Home/Tab1.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { IonContent, IonPage, IonButton, IonGrid, IonRow, IonText, IonIcon, IonPopover } from '@ionic/react';
-import { backspace } from 'ionicons/icons'
+import { IonContent, IonPage, IonButton, IonGrid, IonRow, IonText, IonIcon, IonCol   } from '@ionic/react';
+import { backspace, newspaper } from 'ionicons/icons'
 import './Tab1.css';
 import points from '../../Points/pt-br';
 
@@ -10,28 +10,30 @@ class Tab1 extends Component {
     currentPlayer: "",
     currentWord: "",
     statusGame: true,
+    letterBonus: '',
+    wordBonus: '',
     results: [
       { name: "Paulo", points: 0 },
       { name: 'Daniela', points: 0 }
     ]
   }
 
+  private currentPointWord = 0
+
   componentDidMount(){
     this.setState({letters: points});
-    console.log(points)
   }
 
-  setPoint = (point: number, letter: string) => {
+  setPoint(point: number, letter: string){
     if(this.state.currentPlayer !== '') {
-      const results = this.state.results;
+      if(this.state.letterBonus === 'double') point = point * 2;
+      if(this.state.letterBonus === 'triple') point = point * 3;
       const currentWord = this.state.currentWord;
-      results.map(result => {
-        if(result.name === this.state.currentPlayer) {
-          result.points = result.points + point
-        }
+      this.currentPointWord = this.currentPointWord + point
+      this.setState({
+        currentWord: currentWord + letter,
+        letterBonus: ''
       })
-      this.setState({ results: results, currentWord: currentWord + letter })
-      console.log(this.state.results)
     }
   }
 
@@ -45,14 +47,20 @@ class Tab1 extends Component {
           newPoint = point.value;
         }
       })
+      if(this.state.letterBonus === 'double') newPoint = newPoint * 2;
+      if(this.state.letterBonus === 'triple') newPoint = newPoint * 3;
       players.map(player => {
         if(player.name === this.state.currentPlayer){
           player.points = player.points - newPoint;
         }
       })
-      this.setState({ currentWord: word.substring(0, word.length -1), results: players })
+      this.currentPointWord = this.currentPointWord - newPoint
+      this.setState({ 
+        currentWord: word.substring(0, word.length -1), 
+        results: players,
+        letterBonus: ''
+      })
     }
-    console.log(this.state.results)
   }
 
   showResults(choice: number) {
@@ -74,12 +82,32 @@ class Tab1 extends Component {
         { name: "Paulo", points: 0 },
         { name: 'Daniela', points: 0 }
       ]
-      this.setState({results: results, statusGame: true, currentPlayer: "", currentWord: ""})
+      this.currentPointWord = 0
+      this.setState({results: results, statusGame: true, currentPlayer: "", currentWord: "" })
     }
+    console.log(this.currentPointWord)
   }
 
   setCurrentPlayer(name: string) {
-    this.setState({ currentPlayer: name, currentWord: '' });
+    let results = this.state.results;
+    if(this.state.wordBonus === 'double') this.currentPointWord = this.currentPointWord * 2
+    if(this.state.wordBonus === 'triple') this.currentPointWord = this.currentPointWord * 3
+    results.map(result => {
+      if(result.name === this.state.currentPlayer) {
+        result.points = result.points + this.currentPointWord
+      }
+    })
+    this.currentPointWord = 0
+    this.setState({ currentPlayer: name, currentWord: '', results: results });
+    console.log(this.state.results)
+  }
+
+  setValueWordLetter(type: string, value: string){
+    if(type === 'word') {
+      this.setState({ wordBonus: value })
+    } else if(type === 'letter') {
+      this.setState({ letterBonus: value })
+    }
   }
 
   render() {
@@ -111,35 +139,63 @@ class Tab1 extends Component {
                 <IonText color="dark" style={{ width: '65%', height: 35, backgroundColor: '#EEEEEE'}}>
                   <h4 style={{ marginTop: 5 }}>{this.state.currentWord}</h4>
                 </IonText>
-                <IonButton color="danger" onClick={() => this.removePoint()}>
+                <IonButton disabled={this.state.currentPlayer === ''} color="danger" onClick={() => this.removePoint()}>
                   <IonIcon slot="icon-only" icon={backspace} />
                 </IonButton>
+              </IonRow>
+              <IonRow className="ion-justify-content-center margin5">
+                <IonCol sizeXs="6">
+                  <IonButton
+                    disabled={this.state.currentPlayer === ''}
+                    onClick={() => this.setValueWordLetter('word', 'double')}
+                    color="warning">
+                    Palavra Dupla
+                  </IonButton>
+                </IonCol>
+                <IonCol sizeXs="6">
+                  <IonButton
+                    disabled={this.state.currentPlayer === ''}
+                    onClick={() => this.setValueWordLetter('word', 'triple')}
+                    color="danger">
+                    Palavra Tripla
+                  </IonButton>
+                </IonCol>
               </IonRow>
               <IonRow className="ion-justify-content-center margin10">
                 {points.map((letter, i) => {
                   return(
-                    <div>
-                      <IonButton 
-                        onClick={() => this.setState({ isOpen: true })}
-                        style={{ marginTop: 10, marginRight: 10, marginLeft: 10}}
+                    <IonCol key={i} className="columnLetters" sizeXs="2">
+                      <IonButton
+                        disabled={this.state.currentPlayer === ''}
+                        onClick={() => this.setPoint(letter.value, letter.name)}
                         color="light"
                         key={i}>
                         { letter.name }
-                        <IonPopover 
-                          key={i.toString()} 
-                          isOpen={this.state.isOpen}
-                          cssClass="my-custom-class"
-                          showBackdrop={false}
-                          onDidDismiss={() => this.setState({ isOpen: false })} >
-                        <p>teste</p>
-                      </IonPopover>
                       </IonButton>
-                    </div>
+                    </IonCol>
                   )
                 })}
               </IonRow>
+              <IonRow className="ion-justify-content-center margin5">
+                <IonCol sizeXs="6">
+                  <IonButton
+                    disabled={this.state.currentPlayer === ''}
+                    onClick={() => this.setValueWordLetter('letter', 'double')}
+                    color="secondary">
+                    Letra Dupla
+                  </IonButton>
+                </IonCol>
+                <IonCol sizeXs="6">
+                  <IonButton
+                    disabled={this.state.currentPlayer === ''}
+                    onClick={() => this.setValueWordLetter('letter', 'triple')}>
+                    Letra Tripla
+                  </IonButton>
+                </IonCol>
+              </IonRow>
               <IonRow className="ion-justify-content-center margin20">
-                <IonButton 
+                <IonButton
+                  disabled={this.state.currentPlayer === ''}
                   onClick={() => this.showResults(0)}
                   color="success">
                   Finalizar o Jogo
@@ -165,8 +221,8 @@ class Tab1 extends Component {
               })}
             </IonGrid>
           }
-      </IonContent>
-    </IonPage>
+        </IonContent>
+      </IonPage>
     )
   }
 };
